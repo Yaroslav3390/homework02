a70099040d4f153893096b60bcea9c6e39e1fc75
Yaroslav <yaroslavkutnenko@gmail.com>
2023-05-28 11:59:50 +0300

    second homework completed

--- src/FavoriteColors.java
+++ src/FavoriteColors.java
@@ -1,4 +1,4 @@
-public class Main {
+public class FavoriteColors {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
         System.out.println("Green");

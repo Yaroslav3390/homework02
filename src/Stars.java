971475ca88bf168361ca84f72a7b0db95c3aa85f
Yaroslav <yaroslavkutnenko@gmail.com>
2023-05-30 21:30:09 +0300

    second homework completed

--- src/Stars.java
+++ src/Stars.java
@@ -2,10 +2,12 @@ public class Stars {
 
     public static void main(String[] args) {
 
-        System.out.println("  *");
-        System.out.println(" ***");
+        System.out.println("  *  ");
+        System.out.println(" *** ");
         System.out.println("*****");
-        System.out.println(" ***");
-        System.out.println("  *");
+        System.out.println(" *** ");
+        System.out.println("  *  ");
+
+
     }
 }
